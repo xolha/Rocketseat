adf69b8d57c38c97c1301a15e50fccd699b166d0
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-11-04 12:34:46 -0300

    ✨ feat: update task badges as they are created

--- Full-Stack/React/Projeto 2 - To Do/src/component/tasks-summary.tsx
+++ Full-Stack/React/Projeto 2 - To Do/src/component/tasks-summary.tsx
@@ -2,18 +2,20 @@ import Text from "./ui/text.tsx";
 import Badge from "./ui/badge.tsx";
 
 export default function TasksSummary() {
+    const { taskCount, concludedTasksCount } = useTasks();
+
     return (
         <>
             <div className="flex items-center m-2 gap-1">
                 <Text variant="body-sm-bold" className="text-gray-300">Tarefas criadas</Text>
                 <Badge variant="secondary">
-                    5
+                    {taskCount}
                 </Badge>
             </div>
             <div className="flex items-center m-2 gap-1">
                 <Text variant="body-sm-bold" className="text-gray-300">Concluídas</Text>
                 <Badge variant="primary">
-                    2 de 5
+                    de {taskCount}
                 </Badge>
             </div>
         </>
--- Full-Stack/React/Projeto 2 - To Do/src/hooks/use-task.ts
+++ Full-Stack/React/Projeto 2 - To Do/src/hooks/use-task.ts
@@ -1,12 +1,12 @@
 import useLocalStorage from "use-local-storage";
-import {TASKS_KEY, type Task} from "../models/task.tsx";
+import {TASKS_KEY, type Task, TaskState} from "../models/task.tsx";
 
 export default function useTask() {
     const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
 
     return {
         tasks,
-        tasksCount: tasks.length,
+        tasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
         concludeTaskCount: tasks.filter((task) => task.concluded).length,
     }
 }
