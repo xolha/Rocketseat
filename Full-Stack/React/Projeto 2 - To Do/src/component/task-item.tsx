ae5f8ad8821e3106aa29c7520f0b197cdd4a39d9
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-11-04 03:06:57 -0300

    ✨ feat: add task item component

---
+++ Full-Stack/React/Projeto 2 - To Do/src/component/task-item.tsx
@@ -0,0 +1,53 @@
+import Card from "./ui/card.tsx";
+import InputCheckbox from "./ui/input-checkbox.tsx";
+import Text from "./ui/text.tsx";
+import ButtonIcon from "./ui/button-icon.tsx";
+import TrashIcon from "../assets/icons/Trash-Regular.svg?react";
+import PencilIcon from "../assets/icons/PencilSimple-Regular.svg?react";
+import React from "react";
+import InputText from "./ui/input-text.tsx";
+import XIcon from "../assets/icons/X-Regular.svg?react";
+import CheckIcon from "../assets/icons/Check-Regular.svg?react";
+import Container from "./ui/container.tsx";
+
+export default function TaskItem() {
+    const [isEditing, setEditing] = React.useState();
+
+    function handleEditTask() {
+        // @ts-ignore
+        setEditing(true);
+    }
+
+    function handleExitTask() {
+        // @ts-ignore
+        setEditing(false);
+    }
+
+    return (
+        <Container>
+            <Card size="md" className="flex items-center gap-4">
+                {!isEditing ? /*"não está a editar então mostra o padrão*/
+                    <>
+                        <InputCheckbox/>
+                        <Text variant="flex-1">
+                            🛒 Fazer compras da semana
+                        </Text>
+                        <div className="flex gap-1">
+                            <ButtonIcon icon={TrashIcon} variant="tertiary"/>
+                            <ButtonIcon icon={PencilIcon} variant="tertiary" onClick={handleEditTask}/>
+                        </div>
+                    </>
+                    : (
+                        <>
+                            <InputText className="flex-1"/>
+                            <div className="flex gap-1">
+                                <ButtonIcon icon={XIcon} variant="secondary" onClick={handleExitTask}/>
+                                <ButtonIcon icon={CheckIcon} variant="primary"/>
+                            </div>
+                        </>
+                    )
+                }
+            </Card>
+        </Container>
+    )
+}
--- Full-Stack/React/Projeto 2 - To Do/src/pages/layout-main.tsx
+++ Full-Stack/React/Projeto 2 - To Do/src/pages/layout-main.tsx
@@ -3,11 +3,14 @@ import MainContent from "../component/main-content.tsx";
 import { Outlet} from "react-router";
 import Footer from "../component/footer.tsx";
 import PageHome from "./page-home.tsx";
+import TaskItem from "../component/task-item.tsx";
 
 export default function LayoutMain() {
     return <>
        <Header/>
        <PageHome />
+
+        <TaskItem />
 
         <MainContent>
             <Outlet />
