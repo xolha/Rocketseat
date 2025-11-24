bac7929fff6033df70f64fe726c016729519504a
xolha <juliaportojpa@gmail.com>
2025-11-24 00:07:36 -0300

    💄 style: add assets

--- React Native/Projeto 1 - ComprarApp/src/app/Home/index.tsx
+++ React Native/Projeto 1 - ComprarApp/src/app/Home/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import {styles} from "./styles";
 
 export default function Index() {
