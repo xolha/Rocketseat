0dc7a20da9f765bfa3b6b774f848b9a86caa6acf
xolha <juliaportojpa@gmail.com>
2025-11-25 15:31:25 -0300

    ✨ feat: add and style content to Home page

--- React Native/Projeto 1 - ComprarApp/src/app/Home/index.tsx
+++ React Native/Projeto 1 - ComprarApp/src/app/Home/index.tsx
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Image } from 'react-native';
-import  Button from "@/components/Button";
+import {View, Image} from 'react-native';
+import Button from "@/components/Button";
 import {styles} from "./styles";
 import Input from "@/components/Input";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+    return (
+        <View style={styles.container}>
+            <Image source={require("@/assets/logo.png")} style={styles.logo}/>
 
-        <Input placeholder="O que você quer comprar?" />
-        <Button title="Entrar"/>
-      <StatusBar style="auto" />
-    </View>
-  );
+            <View style={styles.form}>
+                <Input placeholder="O que você quer comprar?"/>
+                <Button title="Entrar"/>
+            </View>
+
+            <View style={styles.content}>
+
+            </View>
+        </View>
+    );
 }
 
--- React Native/Projeto 1 - ComprarApp/src/app/Home/styles.ts
+++ React Native/Projeto 1 - ComprarApp/src/app/Home/styles.ts
@@ -6,11 +6,24 @@ export const styles = StyleSheet.create({
         backgroundColor: "#d0d2d8",
         alignItems: "center",
         paddingTop: 62,
-        padding: 24,
-        gap: 24
     },
     logo: {
         height: 34,
         width: 134,
+    },
+    form: {
+        width: "100%",
+        padding: 16,
+        gap: 7,
+        marginTop: 42,
+    },
+    content: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: "#ffffff",
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        padding: 24,
+        marginTop: 24,
     }
 })
